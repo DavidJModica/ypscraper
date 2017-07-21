d7c702348acea2095d25d23a40b726375b2e7305
David Modica <DavidJModica@gmail.com>
2017-07-21 05:44:52 -0400

    Changed CSV title

--- create-csv.php
+++ create-csv.php
@@ -1,6 +1,3 @@
-
-<html>
-<head>
 <?php
 // Based off of https://github.com/brandon-bailey/yellow-pages-scraper
 include "simple_html_dom.php";
@@ -9,7 +6,7 @@ ini_set('max_execution_time', 300);
 
 //Set headers
 header("Content-type: application/ms-excel");
-header("Content-Disposition: attachment; filename=test.csv");
+header("Content-Disposition: attachment; filename=results.csv");
 
 //Set URL
 $url='https://www.yellowpages.com/search?search_terms=Dog+Day+Care&geo_location_terms=Marblehead%2C+MA';
@@ -65,10 +62,4 @@ foreach($pages as $pageurl) {
 fclose($fp);
 
 ?>
-<title>
-Youtube Scraper</title>
-</head>
-<body>
-The Scraper is creating your CSV now.
-</body>
-</html>
+
